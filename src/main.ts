bbc503e5612562491fe45fea028cf4e25b5a6e6c
sprout2000 <office@nishigami.jp>
2020-04-28 08:49:34 +0900

    refactor: create mkicons.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import stateKeeper from 'electron-window-state';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 
-import fs from 'fs';
 import path from 'path';
 
-import mime from 'mime-types';
-import { setLogger, createICNS, createICO, BICUBIC } from 'png2icons';
-
+import mkicons from './mkicons';
 import template from './menu';
 
 console.log = log.log;
@@ -47,83 +44,7 @@ const createWindow = (): void => {
     },
   });
 
-  ipcMain.handle('dropped-file', async (_e, filepath) => {
-    const mimetype = mime.lookup(filepath);
-
-    if (!mimetype || !mimetype.match(/png/)) {
-      const message = mimetype ? mimetype : 'Unknown';
-
-      await dialog.showMessageBox(win, {
-        type: 'error',
-        buttons: ['OK'],
-        title: 'ERROR',
-        message: 'Error!',
-        detail: `Invalid Format: ${message}.`,
-      });
-
-      return true;
-    }
-
-    const dirname = path.dirname(filepath);
-    if (!dirname) {
-      await dialog.showMessageBox(win, {
-        type: 'error',
-        buttons: ['OK'],
-        title: 'ERROR',
-        message: `Something went wrong...`,
-      });
-
-      return true;
-    }
-
-    const hash = new Date().getTime().toString();
-    const dest = path.join(dirname, `icons-${hash}`);
-
-    await fs.promises
-      .mkdir(dest)
-      .then(async () => {
-        console.log(`create: ${dest}`);
-
-        await fs.promises.readFile(filepath).then(async (buffer) => {
-          setLogger(console.log);
-
-          const icns = createICNS(buffer, BICUBIC, 0);
-          const ico = createICO(buffer, BICUBIC, 0, false);
-
-          await fs.promises
-            .writeFile(path.join(dest, 'icon.icns'), icns)
-            .then(() => console.log(`create: ${dest}${path.sep}icon.icns`));
-
-          await fs.promises
-            .writeFile(path.join(dest, 'icon.ico'), ico)
-            .then(() => console.log(`create: ${dest}${path.sep}icon.ico`));
-        });
-      })
-      .then(async () => {
-        console.log('Successfully Completed!');
-
-        await dialog.showMessageBox(win, {
-          type: 'info',
-          buttons: ['OK'],
-          title: 'Successfully Completed!',
-          message: 'Successfully Completed!',
-          detail: `Created: ${dest}`,
-        });
-      })
-      .catch(async (err) => {
-        console.log(`Something went wrong: ${err}`);
-
-        await dialog.showMessageBox(win, {
-          type: 'error',
-          buttons: ['OK'],
-          title: 'ERROR',
-          message: 'Error!',
-          detail: `Something went wrong: ${err}`,
-        });
-      });
-
-    return true;
-  });
+  ipcMain.handle('dropped-file', (_e, filepath) => mkicons(filepath, win));
 
   win.once('ready-to-show', () => win.show());
 
