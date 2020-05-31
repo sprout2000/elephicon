1e5dd202dee5ee81a69503b4fd4966bc5da1f2ce
sprout2000 <k_konno1983@yahoo.co.jp>
2020-05-31 15:48:56 +0900

    feat: create Success.tsx

---
+++ src/Success.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { IoIosUndo } from 'react-icons/io';
+
+interface Props {
+  onClick: () => void;
+  message: string;
+}
+
+export const Success: React.FC<Props> = (props) => {
+  return (
+    <React.Fragment>
+      <div className="text">Successfully Completed!</div>
+      <div className="result">
+        <div className="filename">{props.message}</div>
+        was created on your desktop.
+      </div>
+      <div className="switch">
+        <div className="back-container" onClick={props.onClick}>
+          <div className="os">
+            <IoIosUndo />
+          </div>
+          <div>Back</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
