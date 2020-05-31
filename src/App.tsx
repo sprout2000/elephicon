d292931a0397498cc0f2a624c2e72a1f46c34fb3
sprout2000 <k_konno1983@yahoo.co.jp>
2020-05-31 17:29:50 +0900

    chore: tmp name to fix case failure

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {
 } from 'react-icons/io';
 
 import { Success } from './Success';
-import { Elephant } from './Elephant';
+import { Elephant } from './AnotherName';
 import { Error } from './Error';
 
 import 'typeface-roboto';
