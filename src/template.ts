865e4113007c9063b048a027402b8679183ebf2f
sprout2000 <k_konno1983@yahoo.co.jp>
2020-06-01 15:53:06 +0900

    refactor: update template for macOS

--- src/template.ts
+++ src/template.ts
@@ -3,6 +3,19 @@ import { app, MenuItemConstructorOptions } from 'electron';
 export const template: MenuItemConstructorOptions[] = [
   {
     label: app.name,
-    role: 'appMenu',
+    submenu: [
+      {
+        role: 'about',
+        accelerator: 'Cmd+I',
+      },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
   },
 ];
