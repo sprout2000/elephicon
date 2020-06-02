59dec645fc1ac5bda97ce18f11ceecb85d0908c1
sprout2000 <k_konno1983@yahoo.co.jp>
2020-06-03 08:29:57 +0900

    chore: add win & appx entry

--- scripts/build.js
+++ scripts/build.js
@@ -3,7 +3,6 @@ const builder = require('electron-builder');
 builder
   .build({
     config: {
-      appId: process.env.APP_BUNDLE_ID,
       productName: 'Elephicon',
       copyright: 'Copyright (C) 2020 sprout2000.',
       artifactName: '${productName}-${version}-${platform}.${ext}',
@@ -15,12 +14,14 @@ builder
         },
       ],
       directories: {
+        buildResources: 'build',
         output: 'release',
       },
       asar: true,
       asarUnpack: ['dist/preload.js'],
       afterSign: 'scripts/notarize.js',
       mac: {
+        appId: process.env.APP_BUNDLE_ID,
         category: 'public.app-category.developer-tools',
         target: ['dmg', 'zip'],
         icon: 'assets/icon.icns',
@@ -47,6 +48,24 @@ builder
       dmg: {
         icon: 'assets/dmg.icns',
         sign: false,
+      },
+      win: {
+        icon: 'assets/icon.ico',
+        target: ['appx'],
+        fileAssociations: [
+          {
+            ext: ['png'],
+            description: 'PNG files',
+          },
+        ],
+      },
+      appx: {
+        applicationId: 'sprout2000.Elephicon',
+        backgroundColor: '#1d3557',
+        displayName: 'Elephicon',
+        identityName: process.env.IDENTITY_NAME,
+        publisher: process.env.PUBLISHER,
+        publisherDisplayName: 'sprout2000',
       },
     },
   })
