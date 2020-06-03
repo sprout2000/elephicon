67a5bac3f9d82b27ed325a8a46c6c0651d46bdd3
sprout2000 <k_konno1983@yahoo.co.jp>
2020-06-04 07:05:05 +0900

    style: add blanks

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -8,23 +8,36 @@ declare global {
 
 export interface Sandbox {
   platform: () => Promise<boolean>;
+
   mimecheck: (filepath: string) => Promise<string | false>;
+
   mkIcns: (filepath: string) => Promise<Result>;
+
   mkIco: (filepath: string) => Promise<Result>;
+
   openDialog: () => Promise<string | void>;
+
   closeWindow: () => void;
+
   openContextMenu: () => void;
+
   onDrop: (
     listener: (_e: Event, filepath: string) => Promise<void>
   ) => Electron.IpcRenderer;
+
   removeOnDrop: () => Electron.IpcRenderer;
+
   menuOpen: (
     listener: (_e: Electron.IpcRendererEvent, filepath: string) => Promise<void>
   ) => Electron.IpcRenderer;
+
   removeMenuOpen: () => Electron.IpcRenderer;
+
   changeState: (arg: boolean) => void;
+
   setState: (
     listener: (_e: Electron.IpcRendererEvent, arg: boolean) => void
   ) => Electron.IpcRenderer;
+
   removeSetState: () => Electron.IpcRenderer;
 }
