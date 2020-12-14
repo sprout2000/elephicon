89d4f760b856554068d098f54514979090f1580b
sprout2000 <wassabie64@gmail.com>
2020-12-15 08:46:25 +0900

    feat: create setLocales.ts

---
+++ src/setLocales.ts
@@ -0,0 +1,15 @@
+import i18next from 'i18next';
+
+import en from './locales/en.json';
+import ja from './locales/ja.json';
+
+export const setLocales = (locale: string): void => {
+  i18next.init({
+    lng: locale,
+    fallbackLng: 'en',
+    resources: {
+      en: { translation: en },
+      ja: { translation: ja },
+    },
+  });
+};
