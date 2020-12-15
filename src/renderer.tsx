28a540053603cf33bf25c95029d633d8207cc689
sprout2000 <wassabie64@gmail.com>
2020-12-15 09:44:30 +0900

    chore: rename imports

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import './styles.scss';
 
-import App from './components/App';
+import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
