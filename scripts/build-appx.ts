bbb74dad42fc1ec3624b6a07338455bd15053e85
sprout2000 <wassabie64@gmail.com>
2020-12-24 09:47:31 +0900

    chore: create build-appx.ts

---
+++ scripts/build-appx.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import { build } from 'electron-builder';
+
+dotenv.config();
+
+build({
+  config: {
+    productName: 'Elephicon',
+    copyright: 'Copyright (C) 2020 sprout2000.',
+    files: ['dist/**/*'],
+    directories: {
+      buildResources: 'assets',
+      output: 'release',
+    },
+    asar: true,
+    asarUnpack: ['dist/preload.js'],
+    win: {
+      icon: 'assets/icon.ico',
+      target: ['appx'],
+      fileAssociations: [
+        {
+          ext: ['png'],
+          description: 'PNG files',
+        },
+      ],
+    },
+    appx: {
+      artifactName: '${productName}-${version}-${platform}.${ext}',
+      applicationId: 'sprout2000.Elephicon',
+      backgroundColor: '#1d3557',
+      displayName: 'Elephicon',
+      showNameOnTiles: true,
+      languages: ['EN-US', 'JA-JP'],
+      identityName: process.env.IDENTITY_NAME,
+      publisher: process.env.PUBLISHER,
+      publisherDisplayName: 'sprout2000',
+    },
+  },
+}).catch((err) => console.log(err));
