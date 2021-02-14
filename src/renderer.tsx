079e1de2de66695edb88ece224f3cff5332c0e1e
sprout2000 <wassabie64@gmail.com>
2021-02-14 16:13:51 +0900

    refactor: rename to components

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import './styles.scss';
 
-import App from './Components/App';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
