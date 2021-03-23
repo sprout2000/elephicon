a748028647762051b52f5b6b1ab78b534e493e40
sprout2000 <wassabie64@gmail.com>
2021-03-23 15:09:52 +0900

    chore: update Switch.test.tsx

--- tests/Switch.test.tsx
+++ tests/Switch.test.tsx
@@ -9,15 +9,28 @@ describe('Switch component', () => {
   test('render Switch component', () => {
     const onClickOS = jest.fn();
 
-    render(<Switch ico={true} loading={false} onClickOS={onClickOS} />);
+    const { rerender } = render(
+      <Switch ico={true} loading={false} onClickOS={onClickOS} />
+    );
 
     expect(screen.getByTestId('ICO')).toBeInTheDocument();
+    expect(screen.getByTestId('ICO')).toHaveClass('icon-container');
+
     expect(screen.getByTestId('ICNS')).toBeInTheDocument();
+    expect(screen.getByTestId('ICNS')).toHaveClass('icon-container unchecked');
 
     userEvent.click(screen.getByTestId('ICO'));
     expect(onClickOS).toBeCalledTimes(1);
 
     userEvent.click(screen.getByTestId('ICNS'));
+    expect(screen.getByTestId('ICNS')).toHaveClass('icon-container');
     expect(onClickOS).toBeCalledTimes(2);
+
+    rerender(<Switch ico={false} loading={false} onClickOS={onClickOS} />);
+    expect(screen.getByTestId('ICO')).toHaveClass('icon-container unchecked');
+
+    rerender(<Switch ico={false} loading={true} onClickOS={onClickOS} />);
+    expect(screen.getByTestId('ICO')).toHaveClass('icon-container loading');
+    expect(screen.getByTestId('ICNS')).toHaveClass('icon-container loading');
   });
 });
