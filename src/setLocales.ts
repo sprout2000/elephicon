448ced413f4fd35a04c4ca304dd6baa007778739
sprout2000 <wassabie64@gmail.com>
2021-04-02 15:15:12 +0900

    feat: Russian localization added

--- src/setLocales.ts
+++ src/setLocales.ts
@@ -5,6 +5,9 @@ import ja from './locales/ja.json';
 /** Merge the pull request sent by DrDeee. */
 /** https://github.com/sprout2000/elephicon/pull/108 */
 import de from './locales/de.json';
+/** Merge the pull request sent by kitt3911. */
+/** https://github.com/sprout2000/elephicon/pull/135 */
+import ru from './locales/ru.json';
 
 export const setLocales = (locale: string): void => {
   i18next.init({
@@ -14,6 +17,7 @@ export const setLocales = (locale: string): void => {
       en: { translation: en },
       ja: { translation: ja },
       de: { translation: de },
+      ru: { translation: ru },
     },
   });
 };
