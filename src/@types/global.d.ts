1d9f800c1cfacfd492f7997976bf356cb750c9b4
sprout2000 <wassabie64@gmail.com>
2021-04-11 08:27:36 +0900

    perf: delete unused APIs

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -9,28 +9,16 @@ declare global {
 export interface Sandbox {
   mimecheck: (filepath: string) => Promise<string | false>;
 
-  mkIcns: (filepath: string) => Promise<Result>;
-
   mkIco: (filepath: string) => Promise<Result>;
 
-  openDialog: () => Promise<string | void>;
+  mkIcns: (filepath: string) => Promise<Result>;
 
-  onDrop: (
-    listener: (_e: Event, filepath: string) => Promise<void>
-  ) => Electron.IpcRenderer;
-  removeOnDrop: () => Electron.IpcRenderer;
+  openDialog: () => Promise<string | void>;
 
   menuOpen: (
     listener: (_e: Electron.IpcRendererEvent, filepath: string) => Promise<void>
   ) => Electron.IpcRenderer;
   removeMenuOpen: () => Electron.IpcRenderer;
-
-  changeICO: (arg: boolean) => void;
-
-  setICO: (
-    listener: (_e: Electron.IpcRendererEvent, arg: boolean) => void
-  ) => Electron.IpcRenderer;
-  removeSetICO: () => Electron.IpcRenderer;
 
   setDesktop: (
     listener: (_e: Electron.IpcRendererEvent, arg: boolean) => void
--- src/preload.ts
+++ src/preload.ts
@@ -5,29 +5,19 @@ contextBridge.exposeInMainWorld('myAPI', {
   mimecheck: async (filepath: string): Promise<string | false> =>
     await ipcRenderer.invoke('mime-check', filepath),
 
-  mkIcns: async (filepath: string): Promise<Result> =>
-    await ipcRenderer.invoke('make-icns', filepath),
-
   mkIco: async (filepath: string): Promise<Result> =>
     await ipcRenderer.invoke('make-ico', filepath),
 
+  mkIcns: async (filepath: string): Promise<Result> =>
+    await ipcRenderer.invoke('make-icns', filepath),
+
   openDialog: async (): Promise<string | void> =>
     await ipcRenderer.invoke('open-file-dialog'),
-
-  onDrop: (listener: (_e: Event, filepath: string) => Promise<void>) =>
-    ipcRenderer.on('dropped', listener),
-  removeOnDrop: () => ipcRenderer.removeAllListeners('dropped'),
 
   menuOpen: (
     listener: (_e: Electron.IpcRendererEvent, filepath: string) => Promise<void>
   ) => ipcRenderer.on('menu-open', listener),
   removeMenuOpen: () => ipcRenderer.removeAllListeners('menu-open'),
-
-  changeICO: (arg: boolean) => ipcRenderer.send('change-ico', arg),
-
-  setICO: (listener: (_e: Electron.IpcRendererEvent, arg: boolean) => void) =>
-    ipcRenderer.once('set-ico', listener),
-  removeSetICO: () => ipcRenderer.removeAllListeners('set-ico'),
 
   setDesktop: (
     listener: (_e: Electron.IpcRendererEvent, arg: boolean) => void
