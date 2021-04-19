629dd9a77dc1bb111d528b0e0685c29ccf8a37ea
sprout2000 <wassabie64@gmail.com>
2021-04-19 09:17:31 +0900

    perf: create BackButton.tsx

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,23 @@
+import React, { useContext, memo } from 'react';
+
+import { AppContext } from '../lib/AppContext';
+
+import { IoIosUndo } from 'react-icons/io';
+
+export const BackButton: React.FC = memo(() => {
+  const { onClickBack } = useContext(AppContext);
+
+  return (
+    <div
+      className="back-container"
+      data-testid="back-container-success"
+      onClick={onClickBack}>
+      <div className="os">
+        <IoIosUndo />
+      </div>
+      <div>Back</div>
+    </div>
+  );
+});
+
+BackButton.displayName = 'BackButton';
