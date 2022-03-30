f1f7bd87a78d11275b705960337278f4b10ec92b
sprout2000 <wassabie64@gmail.com>
2022-03-30 13:30:14 +0900

    feat: Migrated to React 18.

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const mount = document.getElementById('root');
+const root = mount && createRoot(mount);
+
+root?.render(<App />);
