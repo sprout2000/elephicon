c1825b6e763d7d8ab07e9fea3707398e45d1e265
sprout2000 <wassabie64@gmail.com>
2022-04-08 16:21:10 +0900

    chore: Enabled strict mode on development.

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 const mount = document.getElementById('root');
 const root = mount && createRoot(mount);
 
-root?.render(<App />);
+root?.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
