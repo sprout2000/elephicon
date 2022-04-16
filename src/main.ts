e247d2ff22af76b09b03f05d935c0d29b2629e43
sprout2000 <wassabie64@gmail.com>
2022-04-16 17:48:26 +0900

    fix: Fixed devtools on production.

--- src/main.ts
+++ src/main.ts
@@ -154,7 +154,18 @@ const createWindow = () => {
     menu.popup();
   });
 
-  if (isDevelop) mainWindow.webContents.openDevTools({ mode: 'detach' });
+  if (isDevelop) {
+    searchDevtools('REACT')
+      .then((devtools) => {
+        session.defaultSession.loadExtension(devtools, {
+          allowFileAccess: true,
+        });
+      })
+      .catch((err) => console.log(err));
+
+    mainWindow.webContents.openDevTools({ mode: 'detach' });
+  }
+
   mainWindow.loadFile('dist/index.html');
   mainWindow.once('ready-to-show', () => mainWindow.show());
 
@@ -171,16 +182,6 @@ if (!gotTheLock && !isDarwin) {
   app.whenReady().then(() => {
     const locale = app.getLocale();
     setLocales(locale);
-
-    searchDevtools('REACT')
-      .then((devtools) => {
-        if (devtools) {
-          session.defaultSession.loadExtension(devtools, {
-            allowFileAccess: true,
-          });
-        }
-      })
-      .catch((err) => console.log(err));
 
     createWindow();
   });
