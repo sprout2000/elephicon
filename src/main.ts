52113f39df1e68cef3b02aa097c1dd0be4bab915
sprout2000 <wassabie64@gmail.com>
2022-09-22 08:08:49 +0900

    feat: i18n update messages.

--- src/main.ts
+++ src/main.ts
@@ -131,13 +131,15 @@ const createWindow = () => {
     autoUpdater.on('update-available', () => {
       dialog
         .showMessageBox(mainWindow, {
-          message: 'Found Updates',
+          message: i18next.t('Found Updates'),
           detail:
-            'A new version is available.\nDo you want to download it now?',
-          buttons: ['Not now', 'OK'],
+            i18next.t('A new version is available.') +
+            '\n' +
+            i18next.t('Do you want to download it now?'),
+          buttons: [i18next.t('Not now'), 'OK'],
           defaultId: 1,
           cancelId: 0,
-          checkboxLabel: 'No update notifications required.',
+          checkboxLabel: i18next.t('No update notifications required.'),
         })
         .then((result) => {
           if (result.response === 1) {
@@ -161,8 +163,11 @@ const createWindow = () => {
       log.info('Updates downloaded...');
       dialog
         .showMessageBox(mainWindow, {
-          message: 'Install Updates',
-          detail: 'Updates downloaded.\nPlease restart Elephicon...',
+          message: i18next.t('Install Updates'),
+          detail:
+            i18next.t('Updates downloaded.') +
+            '\n' +
+            i18next.t('Please restart Elephicon...'),
         })
         .then(() => {
           setImmediate(() => autoUpdater.quitAndInstall());
