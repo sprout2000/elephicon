60f3f916a8a2d71082f097581a61c770754b8256
sprout2000 <wassabie64@gmail.com>
2022-09-27 12:12:08 +0900

    style: Updated comments and order.

--- src/setLocales.ts
+++ src/setLocales.ts
@@ -1,37 +1,21 @@
 import i18next from 'i18next';
 
+import de from './locales/de.json'; // https://github.com/sprout2000/elephicon/pull/108 by DrDeee
 import en from './locales/en.json';
+import it from './locales/it.json'; // https://github.com/sprout2000/elephicon/pull/140 by LightwithoutLisonlyight
 import ja from './locales/ja.json';
-/** Merge the pull request sent by DrDeee. */
-/** https://github.com/sprout2000/elephicon/pull/108 */
-import de from './locales/de.json';
-/** Merge the pull request sent by kitt3911. */
-/** https://github.com/sprout2000/elephicon/pull/135 */
-import ru from './locales/ru.json';
-/** Merge the pull request sent by godrix. */
-/** https://github.com/sprout2000/elephicon/pull/136 */
-import pt from './locales/pt.json';
-/** Merge the pull request sent by LightwithoutLisonlyight. */
-/** https://github.com/sprout2000/elephicon/pull/140 */
-import it from './locales/it.json';
-/** Merge the pull request sent by ArcherGu. */
-/** https://github.com/sprout2000/elephicon/pull/151 */
-import zh_CN from './locales/zh_cn.json';
-/** Merge the pull request sent by umitseyhan75 */
-/** https://github.com/sprout2000/elephicon/pull/159 */
-import tr from './locales/tr.json';
-/** Merge the pull request sent by aerocyber */
-/** https://github.com/sprout2000/elephicon/pull/163 */
-import ml from './locales/ml.json';
-/** Merge the pull request sent by whitebear60 */
-/** https://github.com/sprout2000/elephicon/pull/223 */
-import uk from './locales/uk.json';
+import ml from './locales/ml.json'; // https://github.com/sprout2000/elephicon/pull/163 by aerocyber
+import pt from './locales/pt.json'; // https://github.com/sprout2000/elephicon/pull/136 by godrix
+import tr from './locales/tr.json'; // https://github.com/sprout2000/elephicon/pull/159 by umitseyhan75
+import ru from './locales/ru.json'; // https://github.com/sprout2000/elephicon/pull/135 by kitt3911
+import uk from './locales/uk.json'; // https://github.com/sprout2000/elephicon/pull/223 by whitebear60
+import zh_CN from './locales/zh_cn.json'; // https://github.com/sprout2000/elephicon/pull/151 by ArcherGu
 
 export const setLocales = (locale: string) => {
   i18next.init({
     lng: locale,
     fallbackLng: 'en',
-    /** see https://source.chromium.org/chromium/chromium/src/+/main:ui/base/l10n/l10n_util.cc */
+    // https://source.chromium.org/chromium/chromium/src/+/main:ui/base/l10n/l10n_util.cc
     resources: {
       de: { translation: de },
       'de-AT': { translation: de },
