3a8651753474b8b25305cf4071b199ebf53f62c7
sprout2000 <wassabie64@gmail.com>
2022-11-06 16:42:22 +0900

    refactor: Updated ReactDOM import.

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
 import { App } from './App';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
